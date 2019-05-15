c1d844c455f5bb94cfef1cf710a7c7bd8a07f7e4
yhjinny <barbie7418@naver.com>
2019-05-15 18:37:18 +0900

    jinny

--- app/src/main/java/com/example/housekeeper_android/ui/activity/DoorActivity.java
+++ app/src/main/java/com/example/housekeeper_android/ui/activity/DoorActivity.java
@@ -1,8 +1,10 @@
 package com.example.housekeeper_android.ui.activity;
 
+import android.content.Context;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Environment;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -12,25 +14,34 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.housekeeper_android.R;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+import static android.media.audiofx.AudioEffect.ERROR;
+
 
 public class DoorActivity extends AppCompatActivity {
 
 
     Toolbar door_toolbar;
+    TextView interphone_outsider_message;
+    EditText interphone_my_message;
+    LinearLayout interphone_my_message_view;
+    Button interphone_capture_btn, interphone_send_message_btn, interphone_send_record_btn;
+    private TextToSpeech tts;
 
     private WebView door_streaming;
 
@@ -39,22 +50,28 @@ public class DoorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_door);
 
-        //툴바 관련
         door_toolbar = (Toolbar) findViewById(R.id.door_toolbar);
+        door_streaming=(WebView)findViewById(R.id.door_webview);
+        interphone_capture_btn=(Button)findViewById(R.id.interphone_capture_btn);
+        interphone_outsider_message=(TextView)findViewById(R.id.interphone_outsider_message);
+        interphone_my_message=(EditText)findViewById(R.id.interphone_my_message);
+        interphone_my_message_view=(LinearLayout)findViewById(R.id.interphone_my_message_view);
+        interphone_send_message_btn=(Button)findViewById(R.id.interphone_send_message_btn);
+        interphone_send_record_btn=(Button)findViewById(R.id.interphone_send_record_btn);
+
+
+        //툴바 관련
         setSupportActionBar(door_toolbar);
         getSupportActionBar().setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.bar_button_return);
 
         //웹뷰 관련
-        door_streaming=(WebView)findViewById(R.id.door_webview);
-
         WebSettings mywebSetting=door_streaming.getSettings();//Mobile Web Setting
         mywebSetting.setJavaScriptEnabled(true);//자바스크립트 허용
        // mywebSetting.setLoadWithOverviewMode(true);//컨텐츠가 웹뷰보다 클 경우 스크린 크기에 맞게 조정
         mywebSetting.setLoadWithOverviewMode(true);
         mywebSetting.setUseWideViewPort(true);
-
 
         door_streaming.setWebViewClient(new WebViewClient(){
             @Override
@@ -64,8 +81,51 @@ public class DoorActivity extends AppCompatActivity {
             }
         });
         door_streaming.loadUrl("http://jsmjsm.iptime.org:8885/?action=stream");
-    }
 
+        //인터폰 외부인 텍스트
+
+
+        //인터폰 사용자 입력 관련 // 사용자가 텍스트 창 누르면 텍스트 자동 초기화
+        interphone_my_message_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                interphone_my_message.setText("");
+                interphone_my_message.setFocusableInTouchMode(true);
+                interphone_my_message.requestFocus();
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+            }
+        });
+
+        //tts 객체 생성
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != ERROR) {
+                    // 언어를 선택한다.
+                    tts.setLanguage(Locale.KOREAN);
+                }
+            }
+        });
+
+
+        //인터폰 사용자 메세지 전송 관련
+        interphone_send_message_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tts.speak(interphone_my_message.getText().toString(),TextToSpeech.QUEUE_FLUSH, null);
+              //  tts.speak(interphone_my_message, TextToSpeech.QUEUE_FLUSH, null);
+
+            }
+        });
+
+        //인터폰 음성녹음 전송 관련
+        interphone_send_record_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+    }
 
     //ToolBar에 menu.xml을 인플레이트함
     @Override
@@ -74,6 +134,18 @@ public class DoorActivity extends AppCompatActivity {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.door_menu, menu);
         return true;
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // TTS 객체가 남아있다면 실행을 중지하고 메모리에서 제거한다.
+        if(tts != null){
+            tts.stop();
+            tts.shutdown();
+            tts = null;
+        }
     }
 
 }
--- app/src/main/java/com/example/housekeeper_android/ui/activity/HomeActivity.java
+++ app/src/main/java/com/example/housekeeper_android/ui/activity/HomeActivity.java
@@ -1,18 +1,57 @@
 package com.example.housekeeper_android.ui.activity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.housekeeper_android.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    Button goto_door, goto_window, goto_record, goto_setting;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        goto_door=(Button)findViewById(R.id.home_goto_door);
+        goto_window=(Button)findViewById(R.id.home_goto_window);
+        goto_record=(Button)findViewById(R.id.home_goto_record);
+        goto_setting=(Button)findViewById(R.id.home_goto_setting);
+
+        goto_door.setOnClickListener(listener);
+        goto_window.setOnClickListener(listener);
+        goto_record.setOnClickListener(listener);
+        goto_setting.setOnClickListener(listener);
+
     }
 
+    Button.OnClickListener listener = new Button.OnClickListener() {
+        public void onClick(View v) {
+            switch(v.getId()){
 
+                case R.id.home_goto_door:
+                    Intent intent = new Intent(HomeActivity.this, DoorActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.home_goto_window:
+                    Intent intent2 = new Intent(HomeActivity.this, WindowActivity.class);
+                    startActivity(intent2);
+                    break;
+                case R.id.home_goto_record:
+                    Intent intent3 = new Intent(HomeActivity.this, RecordActivity.class);
+                    startActivity(intent3);
+                    break;
+                case R.id.home_goto_setting:
+                    Intent intent4 = new Intent(HomeActivity.this, SettingActivity.class);
+                    startActivity(intent4);
+                    break;
+            }
+        }
+    };
 
 }
