66eb1c39b051dd40750e1b832e95d23768e8f7df
minkyoe <ddal0924@naver.com>
2019-07-25 18:01:59 +0900

    MOD window

--- app/src/main/java/com/example/housekeeper_android/ui/Network/Get/GetWindowStatusResponse.java
+++ app/src/main/java/com/example/housekeeper_android/ui/Network/Get/GetWindowStatusResponse.java
@@ -1,5 +1,11 @@
 package com.example.housekeeper_android.ui.Network.Get;
 
 public class GetWindowStatusResponse {
+
     public String responseMessage;
+    int status;
+    public int result;
+
+
+
 }
--- app/src/main/java/com/example/housekeeper_android/ui/Network/Post/PostWindowStatusRequest.java
+++ app/src/main/java/com/example/housekeeper_android/ui/Network/Post/PostWindowStatusRequest.java
@@ -1,4 +1,9 @@
 package com.example.housekeeper_android.ui.Network.Post;
 
 public class PostWindowStatusRequest {
+    Integer w_status;
+
+    public PostWindowStatusRequest(Integer w_status) {
+        this.w_status = w_status;
+    }
 }
