7deeb8641d159b7131945ac9c17ba5228fe352e9
minkyoe <ddal0924@naver.com>
2019-07-30 14:30:42 +0900

    ADD Firebase setting

---
+++ app/src/main/java/com/example/housekeeper_android/ui/FCM/FirebaseInstanceIDService.java
@@ -0,0 +1,16 @@
+package com.example.housekeeper_android.ui.FCM;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.example.housekeeper_android.R;
+
+
+public class FirebaseInstanceIDService {
+
+}
